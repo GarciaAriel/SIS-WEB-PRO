5bffafca67ff946eb4a488fcc83c7ecdb7435cee
AlfaroLore <alfaro.natalia.lorena@gmail.com>
2013-11-28 14:59:16 -0400

    contraseña encriptada

--- control.php
+++ control.php
@@ -1,17 +1,16 @@
 <?php
 $email = $_POST["email"];
 $pass = $_POST["contra"];
-
+$pa=MD5($pass);
 	$db = mysql_connect("localhost", "root", "root");
 	if (!$db)
 	{
-		echo "error en base de datos: ".mysql_error($db);
+		echo "error en base de datoss: ".mysql_error($db);
 	}
 	else 
 	{
 		mysql_select_db("autito",$db);
-		$res = mysql_query("SELECT * FROM usuarios WHERE Email = '$email' and contrasena = '$pass'", $db);
-		
+		$res = mysql_query("SELECT * FROM usuarios WHERE Email = '$email' and Contrasena = '$pa'", $db);
 		if($res != false)
 		{
 			if(mysql_num_rows($res)>0)
@@ -34,13 +33,10 @@ $pass = $_POST["contra"];
 					$_SESSION['pedro'] = $row['Nombre'];
 					header ("Location: administrador.php");	 
 				}
-				  
-
-				
 			}
 			else 
 			{
-				echo "error en base de datos:".mysql_error($db);
+				echo $pa;#"error en base de datoos:".mysql_error($db);
 				mysql_close($db);
 				header("Location: index.php"); //?errorusuario=si
 			}
