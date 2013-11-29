2ad69f584d7bcc5f8e76f01f4da85fe0406c2169
AlfaroLore <alfaro.natalia.lorena@gmail.com>
2013-11-29 11:43:54 -0400

    verificando q funcione el actualizar usuario

--- actualizarDatosUsuario.php
+++ actualizarDatosUsuario.php
@@ -1,25 +1,16 @@
 <?php
 	include ("seguridad.php");
 
-<<<<<<< HEAD
-	$db = mysql_connect("localhost", "root", "root");
-	mysql_select_db("autito",$db);
-	mysql_query("UPDATE usuarios SET Nombre='$nombre', APaterno='$apaterno', AMaterno='$amaterno', Direccion='$direccion', Telefono='$telefono', Carnet='$ci', Email='$email' WHERE Email='$email'");
 
-	
-	header ("Location: usuario.php");	  	
-=======
 		$nombre = $_POST['nombre'];
 		$apaterno = $_POST['apellidop'];
 		$amaterno = $_POST['apellidom'];
 		$direccion = $_POST['direccion'];
 		$telefono = $_POST['telefono'];
 		$ci = $_POST['ci'];
->>>>>>> ed8078ad386c118d7085635df67a377005022a71
-
 		$email = $_SESSION["email"];
 
-		$db = mysql_connect("localhost", "root", "");
+		$db = mysql_connect("localhost", "root", "root");
 		if (!$db)
 		{
 			echo "error en base de datoss: ".mysql_error($db);
