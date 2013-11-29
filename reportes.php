48fcd591f4ff8f64265a6ca65885bf429450ccfa
AlfaroLore <alfaro.natalia.lorena@gmail.com>
2013-11-29 19:06:12 -0400

    reportes pdf finalizado

--- reportes.php
+++ reportes.php
@@ -13,7 +13,7 @@ class PDF extends FPDF
     //Movernos a la derecha
     $this->Cell(80);
     //Título
-    $this->Cell(60,10,'Titulo del archivo',1,0,'C');
+    $this->Cell(60,10,'Reporte de Automoviles',1,0,'C');
     //Salto de línea
     $this->Ln(20);
       
@@ -36,69 +36,32 @@ class PDF extends FPDF
     foreach($header as $col)
     $this->Cell(40,7,$col,1);
     $this->Ln();
-    
-      $this->Cell(40,5,"hola",1);
-      $this->Cell(40,5,"hola2",1);
-      $this->Cell(40,5,"hola3",1);
-      $this->Cell(40,5,"hola4",1);
-      $this->Ln();
-      $this->Cell(40,5,"linea ",1);
-      $this->Cell(40,5,"linea 2",1);
-      $this->Cell(40,5,"linea 3",1);
-      $this->Cell(40,5,"linea 4",1);
+    $db = mysql_connect("localhost", "root", "root");
+    mysql_select_db("autito",$db);
+    $res=mysql_query("SELECT * FROM vehiculos", $db);
+    while($row=mysql_fetch_row($res)){
+      $this->Cell(40,5,$row[0],1);
+      $this->Cell(40,5,$row[2],1);
+      $this->Cell(40,5,$row[9],1);
+      $this->Cell(40,5,$row[1],1);
+      $this->Ln();               
+    }
    }
    
    //Tabla coloreada
-function TablaColores($header)
-{
-  //Colores, ancho de línea y fuente en negrita
-  $this->SetFillColor(255,0,0);
-  $this->SetTextColor(255);
-  $this->SetDrawColor(128,0,0);
-  $this->SetLineWidth(.3);
-  $this->SetFont('','B');
-  //Cabecera
 
-  for($i=0;$i<count($header);$i++)
-  $this->Cell(40,7,$header[$i],1,0,'C',1);
-  $this->Ln();
-  //Restauración de colores y fuentes
-  $this->SetFillColor(224,235,255);
-  $this->SetTextColor(0);
-  $this->SetFont('');
-  //Datos
-     $fill=false;
-  $this->Cell(40,6,"hola",'LR',0,'L',$fill);
-  $this->Cell(40,6,"hola2",'LR',0,'L',$fill);
-  $this->Cell(40,6,"hola3",'LR',0,'R',$fill);
-  $this->Cell(40,6,"hola4",'LR',0,'R',$fill);
-  $this->Ln();
-        $fill=!$fill;
-        $this->Cell(40,6,"col",'LR',0,'L',$fill);
-  $this->Cell(40,6,"col2",'LR',0,'L',$fill);
-  $this->Cell(40,6,"col3",'LR',0,'R',$fill);
-  $this->Cell(40,6,"col4",'LR',0,'R',$fill);
-  $fill=true;
-     $this->Ln();
-     $this->Cell(160,0,'','T');
-}
 
-   
-   
+     
 }
 
 $pdf=new PDF();
 //Títulos de las columnas
-$header=array('Columna 1','Columna 2','Columna 3','Columna 4');
+$header=array('#Placa','Modelo','Categoria','Estado');
 $pdf->AliasNbPages();
 //Primera página
 $pdf->AddPage();
 $pdf->SetY(65);
 //$pdf->AddPage();
 $pdf->TablaSimple($header);
-//Segunda página
-$pdf->AddPage();
-$pdf->SetY(65);
-$pdf->TablaColores($header);
 $pdf->Output();
 ?>
